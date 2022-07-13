4abfbaa4854babe3600cf2219c90f832aa344a06
WILL <dndlf333@naver.com>
2022-07-13 23:14:25 +0900

    flush  -flush 하는 순간 db로 쿼리 문 날림

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -15,8 +15,10 @@ public class JpaMain {
         tx.begin();
         try {
 
-            Member member = em.find(Member.class,150L);
-            member.setName("zzzz");
+            Member member = new Member(200L,"member200");
+            em.persist(member);
+
+            em.flush();
 
             System.out.println("=================================================");
             tx.commit();

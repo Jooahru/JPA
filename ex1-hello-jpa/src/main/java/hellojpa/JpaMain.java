f09d6513a3c0db715c22ee48e50b6ee01fe885ac
WILL <dndlf333@naver.com>
2022-07-13 21:40:13 +0900

    JPA + H2 사용해서 추가 조회 삭제 수정코드

---
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,38 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        try {
+            //등록
+//            Member member = new Member();
+//            member.setId(2L);
+//            member.setName("HelloB");
+            //수정
+//            Member findMember = em.find(Member.class, 1L);
+//            System.out.println("findMember = " + findMember.getId());
+//            System.out.println("findMember = " + findMember.getName());
+//            findMember.setName("HelloJPA");
+            //삭제
+            //em.remove(findMember);
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}

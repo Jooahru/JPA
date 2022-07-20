cd2c40ca94e8d4d2c44c27bf7b0840f9c94a1e9b
WILL <dndlf333@naver.com>
2022-07-20 21:34:06 +0900

    엔티티 매핑
     - 기본 매핑
     - Auto생성 전략 Main 코드

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -16,6 +16,11 @@ public class JpaMain {
         tx.begin();
         try {
 
+            Member member = new Member();
+
+            member.setUsername("C");
+
+            em.persist(member);
 
             tx.commit();
         } catch (Exception e) {
